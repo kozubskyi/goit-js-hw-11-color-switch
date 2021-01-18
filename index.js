2501686c16b1a40a0635b507b202f23e1b805ccb
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-01-19 00:24:29 +0200

    Сделал ДЗ-10.1 (переключатель цветов)

---
+++ index.js
@@ -0,0 +1,49 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+if (localStorage.getItem('body-bg-color') !== null) {
+  refs.body.setAttribute(
+    'style',
+    `background-color: ${localStorage.getItem('body-bg-color')}`,
+  );
+}
+refs.stopBtn.setAttribute('disabled', '');
+let intervalId = 0;
+
+refs.startBtn.addEventListener('click', changeBodyBackground);
+
+function changeBodyBackground() {
+  intervalId = setInterval(() => {
+    let colorIndex = randomIntegerFromInterval(0, colors.length - 1);
+    localStorage.setItem('body-bg-color', colors[colorIndex]);
+    refs.body.style.backgroundColor = colors[colorIndex];
+    console.log(`Выбран цвет ${colorIndex}: ${colors[colorIndex]}`);
+  }, 1000);
+
+  refs.startBtn.setAttribute('disabled', '');
+  refs.stopBtn.removeAttribute('disabled');
+}
+
+refs.stopBtn.addEventListener('click', stopChangingBodyBackground);
+
+function stopChangingBodyBackground() {
+  clearInterval(intervalId);
+  refs.stopBtn.setAttribute('disabled', '');
+  refs.startBtn.removeAttribute('disabled');
+}
